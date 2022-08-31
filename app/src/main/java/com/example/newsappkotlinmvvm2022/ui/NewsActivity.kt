ee5565d2c4721603cd5e2bb2d7e7dac815e8be4f
james <huythuathd@gmail.com>
2022-08-30 23:28:55 -0700

    Hello

---
+++ app/src/main/java/com/example/newsappkotlinmvvm2022/ui/NewsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.newsappkotlinmvvm2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
